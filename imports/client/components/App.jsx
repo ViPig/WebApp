f75417fb6b64b48208a9d59da192cd4fe253779a
osh2ep <osh2ep@gmail.com>
2017-05-21 09:16:52 +0700

    Raw Main ((Form Type:File) + (Responsive Image) Uncompleted)

--- imports/client/components/App.jsx
+++ imports/client/components/App.jsx
@@ -11,12 +11,46 @@ import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 
+// New libs
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Field, reduxForm } from 'redux-form';
+import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+
 import { showLoginModal, hideLoginModal } from '../actions/setLoginModalVisible';
 import { showLeftDrawer, hideLeftDrawer } from '../actions/setLeftDrawerVisible';
 
 import Store from '../store/store';
 // import LoginModal from './Accounts/LoginModal';
 import LeftDrawer from './Drawer/LeftDrawer';
+
+class ContactForm extends Component {
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" component="input" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" component="input" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field name="email" component="input" type="email" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+// Decorate the form component
+ContactForm = reduxForm({
+  form: 'contact', // a unique name for this form
+})(ContactForm);
 
 class Login extends Component {
   static muiName = 'FlatButton';
@@ -61,6 +95,11 @@ class MainMenu extends Component {
   // showLoginModal() {
   //   this.props.showLoginModal(true);
   // }
+
+  // New funcs
+  submit = (values) => {
+    console.log(values);
+  }
   showLeftDrawer() {
     if (!Store.getState().leftDrawer.showLD) {
       this.props.showLeftDrawer(true);
@@ -96,6 +135,38 @@ class MainMenu extends Component {
         <LeftDrawer
           appState={appState}
         />
+        <Grid fluid>
+          <Row className="stats">
+            <Col xs={12} sm md lg>
+              <div>
+                <Paper style={{ height: 100, width: 100, margin: 20, textAlign: 'center', display: 'inline-block' }} zDepth={5}>
+                  <img src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" alt="" />
+                </Paper>
+              </div>
+            </Col>
+            <Col xs={12} sm md lg>
+              <div>
+                <Paper style={{ height: 100, width: 100, margin: 20, textAlign: 'center', display: 'inline-block' }} zDepth={5}>
+                  <img src="http://material-ui.com/images/grid-list/burger-827309_640.jpg" alt="" />
+                </Paper>
+              </div>
+            </Col>
+            <Col xs={12} sm md lg>
+              <div>
+                <Paper style={{ height: 100, width: 100, margin: 20, textAlign: 'center', display: 'inline-block' }} zDepth={5}>
+                  <img src="http://material-ui.com/images/grid-list/camera-813814_640.jpg" alt="" />
+                </Paper>
+              </div>
+            </Col>
+          </Row>
+          <Divider />
+          <Row className="upload">
+            <Col xs>
+              <ContactForm onSubmit={this.submit} />
+            </Col>
+          </Row>
+        </Grid>
+
       </div>
     );
   }
