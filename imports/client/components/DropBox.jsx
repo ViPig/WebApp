5b8227dd15107f86f52b2a02fe41698f455ab767
Tidx <duyttse03672@fpt.edu.vn>
2017-06-29 16:38:26 +0700

    Fix bug

--- imports/client/components/DropBox.jsx
+++ imports/client/components/DropBox.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
-
 import Paper from 'material-ui/Paper';
+
+import Files from '../../../lib/FileCollection';
 
 const style = {
   height: 200,
@@ -37,12 +38,77 @@ export default class DropBox extends React.Component {
       files,
       dropzoneActive: false,
     });
+    this.uploadIt(files);
   }
-
   applyMimeTypes(event) {
     this.setState({
       accept: event.target.value,
     });
+  }
+
+  uploadIt(e) {
+    const self = this;
+
+    if (e) {
+      // We upload only one file, in case
+      // there was multiple files selected
+      const file = e[0];
+
+      if (file) {
+        const uploadInstance = Files.insert({
+          file,
+          meta: {
+            locator: self.props.fileLocator,
+            userId: Meteor.userId(), // Optional, used to check on server for file tampering
+          },
+          streams: 'dynamic',
+          chunkSize: 'dynamic',
+          allowWebWorkers: true, // If you see issues with uploads, change this to false
+        }, false);
+
+        self.setState({
+          uploading: uploadInstance, // Keep track of this instance to use below
+          inProgress: true, // Show the progress bar now
+        });
+
+        // These are the event functions, don't need most of them, it shows where we are in the process
+        uploadInstance.on('start', function () {
+          console.log('Starting');
+        });
+
+        uploadInstance.on('end', function (error, fileObj) {
+          console.log('On end File Object: ', fileObj);
+        });
+
+        uploadInstance.on('uploaded', function (error, fileObj) {
+          console.log('uploaded: ', fileObj);
+
+          // Remove the filename from the upload box
+          // self.refs.fileinput.value = '';
+
+          // Reset our state for the next file
+          self.setState({
+            uploading: [],
+            progress: 0,
+            inProgress: false,
+          });
+        });
+
+        uploadInstance.on('error', function (error, fileObj) {
+          console.log(`Error during upload: ${error}`);
+        });
+
+        uploadInstance.on('progress', function (progress, fileObj) {
+          console.log(`Upload Percentage: ${progress}`);
+          // Update our progress bar
+          self.setState({
+            progress,
+          });
+        });
+
+        uploadInstance.start(); // Must manually start the upload
+      }
+    }
   }
   render() {
     const { accept, files, dropzoneActive } = this.state;
@@ -59,23 +125,25 @@ export default class DropBox extends React.Component {
     };
 
     return (
-      <section>
-        <div className="dropzone">
-          <Dropzone
-            onDrop={this.onDrop.bind(this)}
-          >
-            <p>Drop Here</p>
-          </Dropzone>
-        </div>
-        <aside>
-          <h2>Dropped files</h2>
-          <ul>
-            {
-              this.state.files.map(f => <li>{f.name} - {f.size} bytes</li>)
-            }
-          </ul>
-        </aside>
-      </section>
+      <Dropzone
+        // disableClick
+        // style={{}}
+        // accept={accept}
+        onDrop={this.onDrop.bind(this)}
+        onDragEnter={this.onDragEnter.bind(this)}
+        onDragLeave={this.onDragLeave.bind(this)}
+      >
+        { dropzoneActive && <div style={overlayStyle}>Drop files...</div> }
+        <p>Drop Here</p>
+
+
+        <h2>Dropped files</h2>
+        <ul>
+          {
+            this.state.files.map(f => <li>{f.name} - {f.size} bytes</li>)
+          }
+        </ul>
+      </Dropzone>
     );
   }
 }
